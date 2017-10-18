288f7712935a32c4e7969dd144c259ef10e39b9a
Brendan LE GLAUNEC <brendan.leglaunec@etixgroup.com>
2017-10-18 20:40:16 +0200

    v3.0.0: Add websocket adaptor & pubsub actor - basic server binary

--- server/main.go
+++ server/main.go
@@ -12,5 +12,36 @@
 
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/EtixLabs/cameradar/server/actor/pubsub"
+	"github.com/EtixLabs/cameradar/server/adaptor/websocket"
+	graceful "gopkg.in/tylerb/graceful.v1"
+)
+
 func main() {
+	webSocketFactory := websocket.NewGorillaFactory()
+	pubsub := pubsub.NewWebSocket(webSocketFactory)
+
+	// create and setup the http server
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/", pubsub.Accept)
+
+	httpServer := &graceful.Server{
+		NoSignalHandling: true,
+		Server: &http.Server{
+			Addr:    fmt.Sprintf("%v:%v", "0.0.0.0", 7000),
+			Handler: serverMux,
+		},
+	}
+
+	fmt.Printf("cameradar server listening on %v\n", httpServer.Addr)
+	err := httpServer.ListenAndServe()
+	if err != nil {
+		fmt.Printf("could not start server: %v\n", err)
+		os.Exit(1)
+	}
 }
